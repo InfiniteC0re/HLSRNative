407b6475f6704c60777db240db6658b5f63da570
InfiniteC0re <inf.core0@gmail.com>
2023-02-24 18:55:45 +0500

    Implement FileSystem::CopyDirectory

--- source/Platform/FileSystem_Win.h
+++ source/Platform/FileSystem_Win.h
@@ -39,4 +39,33 @@ public:
 		BOOL bRes = CopyFileW(src, dst, !overwrite);
 		return bRes == TRUE;
 	}
+
+	static bool CopyDirectory(wchar_t* src, wchar_t* dst)
+	{
+		size_t srcLen = wcslen(src);
+		size_t dstLen = wcslen(dst);
+
+		WCHAR* from = new WCHAR[srcLen + 2];
+		wcscpy(from, src);
+		from[srcLen] = L'\0';
+		from[srcLen + 1] = L'\0';
+
+		WCHAR* to = new WCHAR[dstLen + 2];
+		wcscpy(to, src);
+		to[dstLen] = L'\0';
+		to[dstLen + 1] = L'\0';
+
+		SHFILEOPSTRUCTW fileOp = { 0 };
+		fileOp.wFunc = FO_COPY;
+		fileOp.fFlags = FOF_SILENT | FOF_NOCONFIRMMKDIR | FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_NO_UI;
+		fileOp.pFrom = from;
+		fileOp.pTo = to;
+
+		int iRes = SHFileOperationW(&fileOp);
+
+		delete[] from;
+		delete[] to;
+
+		return iRes == 0;
+	}
 };
